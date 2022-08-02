bfe417211fa612586b99752849030511b54d8680
Katherine Jackson (kmj908) <kmj908@mail.usask.ca>
2022-08-02 12:47:40 -0600

    Adds specifc run and minimal files for mturk

--- run_mturk.py
+++ run_mturk.py
@@ -2,8 +2,8 @@ import os
 from BOFS.create_app import create_app
 
 path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "app")
-app = create_app(path, 'minimal.cfg')
+app = create_app(path, 'minimal_mturk.cfg')
 
 if __name__ == '__main__':
     app.debug = False
-    app.run('0.0.0.0', port=8128)
+    app.run('0.0.0.0', port=8129)
